c27ea63472267cd16fbe5c3ec2c1404e339d489c
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-30 07:42:16 +0300

    style: apply code formatting to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/password-generator/',
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    base: "/password-generator/",
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
         @import "./src/styles/global.scss";
         @import "./src/styles/variables.scss";
         @import "./src/styles/mediaQueries.scss";
         `,
-      }
+            },
+        },
     },
-  }
-})
+});
